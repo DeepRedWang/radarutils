4076b27d361bd361ecb756010efaee81722ccaa7
DeepRedWang <wangjianyang@mail.ustc.edu.cn>
2025-07-17 22:20:25 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="radarutils",
-    version="0.1.0",
+    version="0.2.0",
     description="Utility functions for radar data processing",
     author="Jianyang Wang",
     packages=find_packages(),
